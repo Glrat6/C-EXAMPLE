7d2d16718a63f78458ee2584e5f721b61decfe5e
Güler <54442019+Glrat6@users.noreply.github.com>
2020-03-29 01:17:13 +0300

    Add files via upload

---
+++ Character Search.cpp
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+void myStringSearch(const char*,const char ,int *);
+
+int main()
+{
+	char myString[]="aabkaaa";
+	char charToFind='k';
+	
+	int result;
+	myStringSearch(myString,charToFind,&result);
+	
+	if(result!=-1)
+		printf("KARAKTER BULUNDU %d\n",result);
+	else
+		printf("KARAKTER BULUNAMADI.\n");
+}
+void myStringSearch(const char*param1,const char param2,int *sonuc)
+{
+	int i, tut=0;
+	for(;*param1!='\0';param1++)
+	{
+		if(tut==0)
+	   {
+			if(*param1==param2)
+			{
+				 *sonuc=i;
+	              tut++;
+			}
+	        
+		}i++;
+	}
+	
+}
+
